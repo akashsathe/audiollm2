2811c5cfaa604ff453ebba93f8d40320d27af72c
Akash Sathe <akashsathe79@gmail.com>
2025-09-06 01:20:21 +0530

    Initial Django project

--- stt/settings.py
+++ stt/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-61jd&d&m*4^bif8rlt**p+n8-(ttvzadzbhd3urtjxaq%c9ebj'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
